b2edee75779235942374c11a61040194f280b05b
between-zero-and-one-bears <leom52@nycstudents.net>
2025-05-24 12:53:22 -0400

    fix warnings

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,18 @@
 //use rand::prelude::*;
 extern crate macroquad;
 use macroquad::prelude::*;
-use std::borrow::BorrowMut;
 // use macroquad::shapes::* as macroquadshapes;
 extern crate num_traits;
 use num_traits::pow::pow as Pow;
 
 //
-fn RandomNumberBt0and1() -> f64 {0.5}
+fn RandomNumberBt0and1() -> f64 {panic!(); 0.5} // Not used yet
 fn LocalDrawCircle(a: f64, b: f64, c: f64, d: macroquad::prelude::Color) { // called this because it's the local version
 	macroquad::prelude::draw_circle(a as f32, b as f32, c as f32, d)
 }
 
-enum ResultOfFunctionCall {
-	FunctionCallSuccess,
-	FunctionCallFailure
-}
-
-// const UniversalGravitationalConstant: f64 = (6.6743015 / (10 ^ 11));
-const UniversalGravitationalConstant: f64 = 1.0;
+// const UNIVERSAL_GRAVITATIONAL_CONSTANT: f64 = (6.6743015 / (10 ^ 11)); // Commented out to make physics not require huge values for mass to do gravity
+const UNIVERSAL_GRAVITATIONAL_CONSTANT: f64 = 1.0;
 const COLOUR_BLACK: macroquad::color::Color = macroquad::color::Color::new(0.00, 0.00, 0.00, 1.00);
 const COLOUR_WHITE: macroquad::color::Color = macroquad::color::Color::new(1.00, 1.00, 1.00, 1.00);
 
@@ -38,11 +32,11 @@ impl PlanetaryBody {
 	fn SelfAdjustVelocityForGravityToOtherObject(mut self, body_2_r: &PlanetaryBody, delta_time: f64) -> PlanetaryBody {
 		let x_displacement: f64 = body_2_r.location[0] - self.location[0];
 		let y_displacement: f64 = body_2_r.location[1] - self.location[1];
-		let distance: f64 = f64::sqrt(Pow((x_displacement), 2) + Pow((y_displacement), 2));
-		let force: f64 = UniversalGravitationalConstant * self.mass * body_2_r.mass / Pow(distance, 2);
+		let distance: f64 = f64::sqrt(Pow(x_displacement, 2) + Pow(y_displacement, 2));
+		let force: f64 = UNIVERSAL_GRAVITATIONAL_CONSTANT * self.mass * body_2_r.mass / Pow(distance, 2);
 		let vectors: [[f64; 2]; 2] = [[x_displacement / distance, y_displacement / distance], [0.0 - x_displacement / distance, 0.0 - y_displacement / distance]];
-		self.velocity[0] += (delta_time * force * vectors[0][0] / self.mass);
-		self.velocity[1] += (delta_time * force * vectors[0][1] / self.mass);
+		self.velocity[0] += delta_time * force * vectors[0][0] / self.mass;
+		self.velocity[1] += delta_time * force * vectors[0][1] / self.mass;
 		self
 	}
 	fn PairwiseAdjustVelocityForGravity(body_1: &mut PlanetaryBody, body_2: &mut PlanetaryBody, delta_time: f64) {
@@ -50,44 +44,32 @@ impl PlanetaryBody {
 		let y_displacement: f64 = body_2.location[1] - body_1.location[1];
 		let distance: f64 = f64::sqrt(Pow((x_displacement), 2) + Pow((y_displacement), 2));
 		// if distance = (0 as f64) {break} // this should throw an error somehow
-		let force: f64 = UniversalGravitationalConstant * body_1.mass * body_2.mass / Pow(distance, 2);
+		let force: f64 = UNIVERSAL_GRAVITATIONAL_CONSTANT * body_1.mass * body_2.mass / Pow(distance, 2);
 		let vectors: [[f64; 2]; 2] = [[x_displacement / distance, y_displacement / distance], [0.0 - x_displacement / distance, 0.0 - y_displacement / distance]];
 		body_1.velocity[0] = body_1.velocity[0] + (delta_time * force * vectors[0][0] / body_1.mass);
 		body_1.velocity[1] = body_1.velocity[1] + (delta_time * force * vectors[0][1] / body_1.mass);
 		body_2.velocity[0] = body_2.velocity[0] + (delta_time * force * vectors[1][0] / body_2.mass);
 		body_2.velocity[1] = body_2.velocity[1] + (delta_time * force * vectors[1][1] / body_2.mass);
 	}
-
 	fn SelfAdjustLocationForVelocity(self: &mut Self, delta_time: f64) {
 		self.location[0] = self.location[0] + self.velocity[0] * delta_time;
 		self.location[1] = self.location[1] + self.velocity[1] * delta_time;
 	}
-
-	fn PairwiseFindDistanceBetween(body_1: &PlanetaryBody, body_2: &PlanetaryBody) -> f64 {f64::sqrt(Pow((body_1.location[0] - body_2.location[0]), 2) + Pow((body_1.location[1] - body_2.location[1]), 2))}
-	fn PairwiseCheckForCollision(body_1: &PlanetaryBody, body_2: &PlanetaryBody) -> bool {(body_1.radius + body_2.radius) > (PlanetaryBody::PairwiseFindDistanceBetween(body_1, body_2))}
+	fn PairwiseFindDistanceBetween(body_1: &PlanetaryBody, body_2: &PlanetaryBody) -> f64 {f64::sqrt(Pow(body_1.location[0] - body_2.location[0], 2) + Pow(body_1.location[1] - body_2.location[1], 2))}
+	fn PairwiseCheckForCollision(body_1: &PlanetaryBody, body_2: &PlanetaryBody) -> bool {body_1.radius + body_2.radius < PlanetaryBody::PairwiseFindDistanceBetween(body_1, body_2)}
 } // this is the end of the impl block
-
 
 fn PhysicsTick(mut planetary_bodies: Vec::<PlanetaryBody>, delta_time: f64) -> Vec::<PlanetaryBody> {
 	//'collision_checks: loop {break 'collision_checks;} // check and handle collisions. break added temporarily, commented out for skipping initially
 	let number_of_bodies: usize = planetary_bodies.len();
 	for index in 0..(number_of_bodies - 1) {
 		planetary_bodies[index].SelfAdjustLocationForVelocity(delta_time)
-		//if let Some(body) = planetary_bodies_mr.get_mut(index){
-		//	body.SelfAdjustLocationForVelocity(delta_time);
-		//};
 	}
 	for first_index in 1..(number_of_bodies-1) {
 		for second_index in first_index..number_of_bodies {
 			planetary_bodies[first_index] = planetary_bodies[first_index].clone().SelfAdjustVelocityForGravityToOtherObject(&planetary_bodies[second_index], delta_time);
 			planetary_bodies[second_index] = planetary_bodies[second_index].clone().SelfAdjustVelocityForGravityToOtherObject(&planetary_bodies[first_index], delta_time);
-			//PlanetaryBody::PairwiseAdjustVelocityForGravity(planetary_bodies.get_mut(first_index).unwrap(), planetary_bodies.get_mut(second_index).unwrap(), delta_time);
-			//let tupbodies: (Option::<&mut PlanetaryBody>, Option::<&mut PlanetaryBody>) = (planetary_bodies_mr.get_mut(second_index), planetary_bodies_mr.get_mut(first_index - 1))
-			//if let (Some(second_body), Some(first_body)) = tupbodies {
-			//	PlanetaryBody::PairwiseAdjustVelocityForGravity(first_body, second_body, delta_time);
-			//};
-		};
-		//if number_of_bodies - lower_index == 1 
+		}; 
 	};
 	return planetary_bodies
 }
@@ -97,6 +79,12 @@ fn RenderBodies(planetary_bodies_r: &Vec<PlanetaryBody>, view_attributes: [f64; 
 		LocalDrawCircle(item.location[0] * view_attributes[2] + view_attributes[0], item.location[1] * view_attributes[2] + view_attributes[1], item.radius * view_attributes[2], COLOUR_BLACK)
 	}
 }
+
+//
+
+//
+
+//
 
 #[macroquad::main("Assignment-Gravity-Sim")]
 async fn main() {  // This is the function that is normally set to immediately execute on starting the program. 
